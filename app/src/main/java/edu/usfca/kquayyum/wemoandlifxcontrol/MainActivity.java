3100b7649f48bbb33c4ef3e95d11a987e9e7a53f
Kaynat Quayyum <kquayyum@dons.usfca.edu>
2016-08-08 13:49:13 -0700

    old wemo turn on-off deleted

--- app/src/main/java/edu/usfca/kquayyum/wemoandlifxcontrol/MainActivity.java
+++ app/src/main/java/edu/usfca/kquayyum/wemoandlifxcontrol/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
